6aae7b4dce5220e2c3f228409991ffde11ff5f6f
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-10 17:54:53 -0300

    Requisito 2 completo.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  /*
+  Achar qual espécie têm o mesmo nome que o parâmetro.
+  Da espécie (item do array dos dados) acessar cada residente, e de cada residente acessar cada idade. Verificar se idade maior ou igual que parâmetro.
+  Retornar booleano.
+  */
+
+  let specieName = data.species.find(specieFind => specieFind.name === animal);
+
+  return specieName.residents.every(resident => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
