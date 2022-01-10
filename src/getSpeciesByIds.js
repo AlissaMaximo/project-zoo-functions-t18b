f8cef80278ed152b3a2c7a25ae265a38ac33039f
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-10 16:45:02 -0300

    Requisito 1 completo.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  /*
+  Buscar as espécies pelo id - percorrer o array de espécies. Cada espécie é um objeto.
+  Pegar o objeto pelo id. Se no objeto a condição der true, retorna na HOF o objeto inteiro (item do array).
+  Ter um array com as espécies p cada id.
+  No final, retornar esse array.
+  Lilian Azevedo me auxiliou.
+  */
+
+  let speciesArr = data.species.filter( itemFilter => itemFilter.id === ids.find( (itemFind) => itemFind === itemFilter.id) );
+
+  return speciesArr;
 }
 
 module.exports = getSpeciesByIds;
