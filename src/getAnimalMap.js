3ac70678e295da849ef5eaf2dcf79cff2f9de814
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-16 04:20:03 -0300

    Tentativa de Requisito 6 item 3.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,8 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 // Primeiro item.
 function getOnlyByLocation() {
-  return data.species.reduce((acc, cur) => {
+  return species.reduce((acc, cur) => {
     const { name, location } = cur;
 
     if (acc[location] === undefined) {
@@ -14,10 +14,61 @@ function getOnlyByLocation() {
   }, {});
 }
 
+const expected = {
+  NE: [
+    { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] },
+    { giraffes: ['Gracia', 'Antone', 'Vicky', 'Clay', 'Arron', 'Bernard'] },
+  ],
+  NW: [
+    { tigers: ['Shu', 'Esther'] },
+    { bears: ['Hiram', 'Edwardo', 'Milan'] },
+    { elephants: ['Ilana', 'Orval', 'Bea', 'Jefferson'] },
+  ],
+  SE: [
+    { penguins: ['Joe', 'Tad', 'Keri', 'Nicholas'] },
+    { otters: ['Neville', 'Lloyd', 'Mercedes', 'Margherita'] },
+  ],
+  SW: [
+    { frogs: ['Cathey', 'Annice'] },
+    { snakes: ['Paulette', 'Bill'] },
+  ],
+};
+
+function getByName() {
+  const organizedAnimals = species.reduce((accSpc, curSpc) => {
+    const { name, location } = curSpc;
+    const { residents } = curSpc;
+
+    if (accSpc[location] === undefined) {
+      accSpc[location] = [];
+    }
+
+    let specie = {};
+    specie[name] = [];
+
+    //rever este segundo reduce
+    curSpc.residents.reduce((accRes, curRes) => {
+      const resName = curRes.name;
+
+      specie[name].push(resName);
+
+      return accRes;
+    }, [])
+
+    accSpc[location].push(specie);
+
+    return accSpc;
+  }, {})
+  return organizedAnimals
+}
+
 function getAnimalMap(options) {
   if (options === undefined || options.includeNames === undefined) {
     return getOnlyByLocation();
   }
+  return getByName();
 }
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
