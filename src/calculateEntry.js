83045f9232dadf558f2c351aee50e7175c820124
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-16 00:16:19 -0300

    Requisito 5: countEntrants() completo.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,41 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const counted = entrants.reduce((acc, cur) => {
+    const { age } = cur;
+
+    if (age < 18) {
+      if (acc.child === undefined) {
+        acc.child = 0;
+      }
+      acc.child += 1;
+    } else if (age >= 50) {
+      if (acc.senior === undefined) {
+        acc.senior = 0;
+      }
+      acc.senior += 1;
+    } else {
+      if (acc.adult === undefined) {
+        acc.adult = 0;
+      }
+      acc.adult += 1;
+    }
+
+    return acc;
+  }, {})
+
+  return counted;
 }
+const entradas = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+countEntrants(entradas);
 
 function calculateEntry(entrants) {
   // seu código aqui
