92f21d373d45e5e38f5cf20ca66b2e23f6b07ff7
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-16 15:49:35 -0300

    Requisito 7 com terceiro item completo.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -14,53 +14,23 @@ function getOnlyByLocation() {
   }, {});
 }
 
-const expected = {
-  NE: [
-    { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] },
-    { giraffes: ['Gracia', 'Antone', 'Vicky', 'Clay', 'Arron', 'Bernard'] },
-  ],
-  NW: [
-    { tigers: ['Shu', 'Esther'] },
-    { bears: ['Hiram', 'Edwardo', 'Milan'] },
-    { elephants: ['Ilana', 'Orval', 'Bea', 'Jefferson'] },
-  ],
-  SE: [
-    { penguins: ['Joe', 'Tad', 'Keri', 'Nicholas'] },
-    { otters: ['Neville', 'Lloyd', 'Mercedes', 'Margherita'] },
-  ],
-  SW: [
-    { frogs: ['Cathey', 'Annice'] },
-    { snakes: ['Paulette', 'Bill'] },
-  ],
-};
-
 function getByName() {
   const organizedAnimals = species.reduce((accSpc, curSpc) => {
     const { name, location } = curSpc;
+    const accumulatorSpecie = accSpc;
+    const specie = {};
+    specie[name] = [];
 
-    if (accSpc[location] === undefined) {
-      accSpc[location] = [];
+    if (accumulatorSpecie[location] === undefined) {
+      accumulatorSpecie[location] = [];
     }
-
-    let specie = {};
-
-    //rever este segundo reduce
-    specie[name] = curSpc.residents.reduce((accRes, curRes) => {
-      const resName = curRes.name;
-      /* console.log('resName: '+resName); // cada vez mostra o nome do residente, de todos os animais.
-      console.log('specie name: '+name); //mostra a espécie atual.
-      console.log(accSpc); //mostra a ideia do objeto final.
-      console.log(specie); //mostra o objeto da espécie atual.*/
-      accRes.push(resName);
-      /* console.log(accRes); //mostra no final do ciclo um array com os nomes de cada residente para cada espécie. */
-      return accRes;
-    }, [])
-
-    accSpc[location].push(specie);
-
-    return accSpc;
-  }, {})
-  return organizedAnimals
+    curSpc.residents.forEach((resident) => {
+      specie[name].push(resident.name);
+    });
+    accumulatorSpecie[location].push(specie);
+    return accumulatorSpecie;
+  }, {});
+  return organizedAnimals;
 }
 
 function getAnimalMap(options) {
@@ -73,7 +43,6 @@ function getAnimalMap(options) {
 console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
-
 
 /*
 Referência para getByName():
